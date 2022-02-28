226bbddec9e4854e29db731ed6320dff5cf782d5
hubertdepsv <60505590+hubertdepsv@users.noreply.github.com>
2022-02-28 20:53:23 +0100

    Added a new Compound folder with refactored code

---
+++ Compound/query_main.py
@@ -0,0 +1,32 @@
+import pprint
+
+import run_query as rn
+import get_markets_mass as gmm
+
+granularity_of_data = "d" # d - day
+pp = pprint.PrettyPrinter(indent=2)
+
+query = rn.query('''query a($blockNumber: Int!){
+        markets(block:{number:$blockNumber}){
+            underlyingName
+            symbol
+            cash
+            collateralFactor
+            exchangeRate
+            supplyRate
+            totalBorrows
+            totalSupply
+            blockTimestamp
+            underlyingPriceUSD
+            underlyingPrice
+            borrowRate
+            reserveFactor
+            borrowIndex
+            exchangeRate
+        }
+    }
+''')
+
+url = "https://api.thegraph.com/subgraphs/name/graphprotocol/compound-v2"
+
+gmm.get_data(query=query, granularity_of_data=granularity_of_data)
