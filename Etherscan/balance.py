798400368b86eb60af5bc3877a4318f9fe42871c
hubertdepsv <60505590+hubertdepsv@users.noreply.github.com>
2022-06-07 15:39:01 +0200

    added a function to download 10,000 rows and save them in a .feather file

--- Etherscan/balance.py
+++ Etherscan/balance.py
@@ -1,6 +1,7 @@
 from requests import get
 from matplotlib import pyplot as plt
 from datetime import datetime
+import pandas as pd
 
 API_KEY = "QH3NS55JXZ72B9VTZ6WBJ83BHVZVD84K4A"
 ETH_VALUE = 10 ** 18
@@ -22,6 +23,40 @@ def get_account_balance(address):
     return value
 
 def get_transactions(address):
+
+    transactions_url = make_api_url("account", "txlist", address, startblock=0, endblock=99999999, page=1, offset=10000, sort="asc")
+    response = get(transactions_url)
+    data = response.json()["result"]
+
+    internal_tx_url = make_api_url("account", "txlistinternal", address, startblock=0, endblock=99999999, page=1, offset=10000, sort="asc")
+    response2 = get(internal_tx_url)
+    data2 = response2.json()["result"]
+
+    data.extend(data2)
+    data.sort(key=lambda x: int(x["timeStamp"]))
+
+    lst = []
+
+    for tx in data:
+        lst_tx = []
+        lst_tx.append(tx["to"])
+        lst_tx.append(tx["from"])
+        lst_tx.append(int(tx["value"]) / ETH_VALUE)
+        if "gasPrice" in tx:
+            gas = (int(tx["gasUsed"]) * int(tx["gasPrice"])) / ETH_VALUE
+        else:
+            gas = int(tx["gasUsed"]) / ETH_VALUE
+        lst_tx.append(gas)
+        lst_tx.append(datetime.fromtimestamp(int(tx["timeStamp"])))
+        lst_tx.append(tx["nonce"])
+        lst.append(lst_tx)
+
+    columns = ['to', 'from', 'value', 'gas', 'time', 'nonce']
+    df = pd.DataFrame(lst, columns=columns)
+    df.to_feather('maker1.feather')
+
+
+def get_transactions_graph(address):
     """Normal transactions + internal transactions"""
     transactions_url = make_api_url("account", "txlist", address, startblock=0, endblock=99999999, page=1, offset=10000, sort="asc")
     response = get(transactions_url)
@@ -60,6 +95,5 @@ def get_transactions(address):
     plt.plot(times, balances)
     plt.show()
 
-address = "0xde0b295669a9fd93d5f28d9ec85e40f4cb697bae"
-
+address = "0x740f8B58f5562C8379F2A8c2230C9be5C03Ac3Fc"
 get_transactions(address)
